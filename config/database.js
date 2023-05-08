965b126510cac1a9aa358778b4c9ecb3a83164f7
Putra22566008 <22566006@stmik.jayakarta.ac.id>
2023-05-08 21:09:26 +0700

    first commit

---
+++ config/database.js
@@ -0,0 +1,15 @@
+const mysql = require('mysql');
+// buat konfigurasi koneksi
+const koneksi = mysql.createConnection({
+   host: 'localhost',
+   user: 'root',
+   password: '',
+   database: 'akademik',
+   multipleStatements: true
+});
+// koneksi database
+koneksi.connect((err) => {
+   if (err) throw err;
+   console.log('MySQL Connected...');
+});
+module.exports = koneksi;
